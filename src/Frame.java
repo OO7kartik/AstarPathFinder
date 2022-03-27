b9887009be52ce4deec6d24f7dd9151cd19d9ef2
Rohit Kartik <rohit.audrey@gmail.com>
2022-03-27 23:08:39 +0530

    feat(Frame): setup structure of painComponent

--- src/Frame.java
+++ src/Frame.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Frame extends JPanel {
+public class Frame extends JPanel implements ActionListener, MouseListener, MouseMotionListener, MouseWheelListener, KeyListener {
 
     JFrame window;
     AStarPathFinder pathFinder;
     int size;
 
     Node startNode, endNode;
+
+    Timer timer = new Timer(100, this);
+    int R = randomPixelIntensity(0, 255);
+    int G = randomPixelIntensity(0, 255);
+    int B = randomPixelIntensity(0, 255);
 
     public static void main(String[] args) {
         System.out.println("Creating new frame....");
@@ -17,6 +23,14 @@ public class Frame extends JPanel {
 
     public Frame() {
         size = 25;
+
+        setLayout(null);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+
 
         pathFinder = new AStarPathFinder(this, size);
 
@@ -28,5 +42,28 @@ public class Frame extends JPanel {
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+
+        // PHASE 2
+        // TODO: handle no path
+        // TODO: handle found path
+
+
+        // PHASE 1
+        // TODO: draw grid
+        // TODO: draw obstacles
+        // TODO: draw open nodes
+        // TODO: draw closed nodes
+        // TODO: draw final path nodes
+        // TODO: draw start path
+        // TODO: draw end path
+    }
+
+    public int randomPixelIntensity(int rangeStart, int rangeEnd) {
+        return (int) (Math.random() * (rangeEnd - rangeStart + 1)) + rangeStart;
     }
 }
