6d7699c75bbc1c3f74901e671ab928b5fb172c30
Rohit Kartik <rohit.audrey@gmail.com>
2022-03-30 10:21:42 +0530

    feat(Frame): add setup for handling key and mouse Events

--- src/Frame.java
+++ src/Frame.java
@@ -149,4 +149,58 @@ public class Frame extends JPanel implements ActionListener, MouseListener, Mous
         repaint();
     }
 
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+
+    }
 }
